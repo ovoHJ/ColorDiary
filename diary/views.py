52ad97e6cc7762fa56bddf7ab13e67affe0c8577
Hyejeong <s2018w01@e-mirim.hs.kr>
2020-07-20 03:08:20 +0900

    diary/views DiaryListView, DiaryCreateView, DiaryUpdateView, DiaryDeleteView

--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
 
-# Create your views here.
+from diary.models import Diary
+
+
+class DiaryListView(ListView):
+    model = Diary
+
+
+class DiaryCreateView(CreateView):
+    model = Diary
+    fields = '__all__'
+    template_name = 'diary/diary_create.html'
+    success_url = reverse_lazy('diary:list')
+
+
+class DiaryUpdateView(UpdateView):
+    model = Diary
+    fields = '__all__'
+    template_name_suffix = '_update'
+    success_url = reverse_lazy('diary:list')
+
+
+class DiaryDeleteView(DeleteView):
+    model = Diary
+    success_url = reverse_lazy('diary:list')
