513a1c3e4f84d64420957dc17fe5b3781057de9f
Hyejeong <s2018w01@e-mirim.hs.kr>
2020-07-20 02:11:27 +0900

    ColorDiary/settings colorfield, diary/models Diary

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,10 @@
+from colorfield.fields import ColorField
 from django.db import models
 
-# Create your models here.
+
+class Diary(models.Model):
+    title = models.CharField(max_length=50)
+    contents = models.CharField(max_length=255)
+    name = models.CharField(max_length=10)
+    date = models.DateTimeField(auto_now_add=True)
+    color = ColorField(default='#FF0000')
