7ab7674a22989a67c5dc751a8ef5e09ac4714fbb
Hyejeong <s2018w01@e-mirim.hs.kr>
2020-07-20 02:38:00 +0900

    diary/admin Diary, diary/models __str__

--- diary/models.py
+++ diary/models.py
@@ -8,3 +8,6 @@ class Diary(models.Model):
     name = models.CharField(max_length=10)
     date = models.DateTimeField(auto_now_add=True)
     color = ColorField(default='#FF0000')
+
+    def __str__(self):
+        return f'제목 : {self.title}, 내용 : {self.contents}, 이름 : {self.name}, 날짜 : {self.date}, 색상 : {self.color}'
