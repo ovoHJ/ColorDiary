5604156adb93d95b000902e01606a64a1ddcfede
Hyejeong <s2018w01@e-mirim.hs.kr>
2020-07-20 03:17:38 +0900

    diary/urls create, update, delete

--- diary/urls.py
+++ diary/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from diary.views import DiaryListView
+from diary.views import DiaryListView, DiaryCreateView, DiaryUpdateView, DiaryDeleteView
 
 app_name = 'diary'
 
 urlpatterns = [
     path('', DiaryListView.as_view(), name='list'),
+    path('create/', DiaryCreateView.as_view(), name='create'),
+    path('update/<int:pk>/', DiaryUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', DiaryDeleteView.as_view(), name='delete'),
 ]
